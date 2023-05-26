dfd74f54f71b2ab357c9a373e248b292616475b9
ronakpustack <ronak@pustack.com>
2023-05-26 16:54:22 +0530

    Added notification receiver callbacks

--- lib/state/download_state.dart
+++ lib/state/download_state.dart
@@ -1,3 +1,8 @@
+import 'dart:async';
+import 'dart:isolate';
+import 'dart:ui';
+
+import 'package:downloads_module/constants/constants.dart';
 import 'package:downloads_module/model/download_item.dart';
 import 'package:downloads_module/utils/custom_exception.dart';
 import 'package:downloads_module/utils/utils.dart';
@@ -6,6 +11,9 @@ import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:path_provider/path_provider.dart';
 
 class DownloadState extends ChangeNotifier {
+  final ReceivePort _receivePort = ReceivePort();
+  StreamSubscription? _portStateUpdates;
+
   Future<String> _findDirectory() async {
     try {
       String externalStorageDirPath;
@@ -34,5 +42,40 @@ class DownloadState extends ChangeNotifier {
     } catch (e) {
       throw CustomException("onDownloadButtonTap: $e");
     }
+  }
+
+  _onReceiveData(data, {required BuildContext context}) async {
+    if (data == null) {
+      return;
+    }
+
+    String? id = data[0];
+    int? status = data[1];
+    int? progress = data[2];
+
+    print("On receive data | Status: $status");
+  }
+
+  // Binds the background isolate
+  void bindBackgroundIsolate({required BuildContext context}) {
+    bool isSuccess = IsolateNameServer.registerPortWithName(
+      _receivePort.sendPort,
+      kDownloadsPort,
+    );
+
+    if (!isSuccess) {
+      _unbindBackgroundIsolate();
+      bindBackgroundIsolate(context: context);
+      return;
+    }
+
+    _portStateUpdates = _receivePort.listen((dynamic data) {
+      _onReceiveData(data, context: context);
+    });
+  }
+
+  // Unbinds the background isolate
+  void _unbindBackgroundIsolate() {
+    IsolateNameServer.removePortNameMapping(kDownloadsPort);
   }
 }
