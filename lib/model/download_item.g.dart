642be8d0eb5f1dcb1e965f680c7e67c8432da9b1
ronakpustack <ronak@pustack.com>
2023-05-29 23:13:21 +0530

    Generated Hive Adapter

---
+++ lib/model/download_item.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'download_item.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class DownloadItemAdapter extends TypeAdapter<DownloadItem> {
+  @override
+  final int typeId = 0;
+
+  @override
+  DownloadItem read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return DownloadItem(
+      id: fields[0] as String,
+      title: fields[1] as String,
+      url: fields[2] as String,
+    )
+      ..savedFilePath = fields[4] as String?
+      ..fileName = fields[5] as String?;
+  }
+
+  @override
+  void write(BinaryWriter writer, DownloadItem obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.url)
+      ..writeByte(4)
+      ..write(obj.savedFilePath)
+      ..writeByte(5)
+      ..write(obj.fileName);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DownloadItemAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
