2afd95a580c5b9033cbb7f2b617f65ef0ee8f3d4
ronakpustack <ronak@pustack.com>
2023-05-29 23:14:02 +0530

    Enhanced DetailPage to work well with downloaded items

--- lib/screens/detail_page.dart
+++ lib/screens/detail_page.dart
@@ -2,6 +2,7 @@ import 'package:better_player/better_player.dart';
 import 'package:downloads_module/screens/widgets/download_button.dart';
 import 'package:downloads_module/screens/widgets/pdf_viewer.dart';
 import 'package:downloads_module/state/download_state.dart';
+import 'package:downloads_module/state/downloads_provider.dart';
 import 'package:flutter/material.dart';
 
 import 'package:downloads_module/enum/download_item_type.dart';
@@ -31,25 +32,36 @@ class _DetailPageState extends State<DetailPage> {
   void initState() {
     super.initState();
 
-    Provider.of<DownloadState>(context, listen: false)
-        .intialize(downloadItem: widget.item);
+    if (!widget.item.isDownloaded) {
+      Provider.of<DownloadState>(context, listen: false)
+          .intialize(downloadItem: widget.item);
+    }
   }
 
   Widget _getView() {
     switch (widget.item.downloadItemType) {
       case DownloadItemType.image:
-        return Image.network(widget.item.url);
+        return widget.item.isDownloaded
+            ? Image.file(widget.item.getFile)
+            : Image.network(widget.item.url);
       case DownloadItemType.video:
         return Column(
           children: [
             AspectRatio(
               aspectRatio: 16 / 9,
-              child: BetterPlayer.network(widget.item.url),
+              child: widget.item.isDownloaded
+                  ? BetterPlayer.file(widget.item.savedFilePath!)
+                  : BetterPlayer.network(widget.item.url),
             ),
           ],
         );
       case DownloadItemType.pdf:
-        return PDFViewer(url: widget.item.url);
+        return PDFViewer(
+          isLocal: widget.item.isDownloaded,
+          url: widget.item.isDownloaded
+              ? widget.item.savedFilePath!
+              : widget.item.url,
+        );
       case DownloadItemType.apk:
         return Column(
           children: [
@@ -87,15 +99,35 @@ class _DetailPageState extends State<DetailPage> {
       appBar: AppBar(
         title: Text(widget.item.title),
         actions: [
-          DownloadButton(item: widget.item),
+          if (widget.item.isDownloaded)
+            IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () async {
+                Navigator.pop(context);
+                await Provider.of<DownloadsProvider>(context, listen: false)
+                    .removeFromDownloads(
+                  widget.item,
+                  context: context,
+                );
+              },
+            )
+          else
+            DownloadButton(item: widget.item),
         ],
       ),
       body: Column(
         children: [
-          _getView(),
-          Text(
-            "downloading: ${Provider.of<DownloadState>(context).currentItemDownloadTask?.url}",
-          )
+          Expanded(
+            child: _getView(),
+          ),
+          if (widget.item.isDownloaded)
+            Text(
+              "File Path: ${widget.item.savedFilePath}",
+            )
+          else
+            Text(
+              "URL: ${Provider.of<DownloadState>(context).currentItemDownloadTask?.url}",
+            )
         ],
       ),
     );
